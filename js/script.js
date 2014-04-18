2cf34931ec7e8109298ac120b94230555bd7aef7
puhrome <phirom.yim@fullsail.edu>
2014-04-17 22:12:18 -0400

    Tire Pressure I
    Setting up arrays in WebStorm. Attempted to write a conditional with
    the data from the arrays. Still need to practice.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Tire Pressure I Created by Phirom Yim on 4/17/14.
+ */
+//What are the Tire Pressures of each tire
+//create an array to hold all 4 tire pressures
+var tirePressure = [2.52, 3.44, 3.44, 2.43];
+tirePressure [0] = "Front Right";
+tirePressure [1] = "Front Left";
+tirePressure [2] = "Rear Right";
+tirePressure [3] = "Rear Left";
+
+//still not comfortable with arrays
+//need to practice more of this
